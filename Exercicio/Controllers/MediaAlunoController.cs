ef026199b7ff48b174e54865a55f83a5582b13b8
ITALO QUEIROZ FERNANDES DE SOUSA <italo.qfsousa@senacsp.edu.br>
2022-10-21 20:21:09 -0300

    MEDIA

---
+++ Exercicio/Controllers/MediaAlunoController.cs
@@ -0,0 +1,39 @@
+﻿using Exercicio.DTO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Exercicio.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MediaAlunoController : ControllerBase
+    {
+        [HttpPost]
+
+        public ActionResult<MediaAlunoDTO> Post(string aluno,double nota1,double nota2, double nota3, double nota4)
+        {
+
+            var resultado = new MediaAlunoDTO();
+
+            var media = (nota1 + nota2 + nota3 + nota4) / 4 
+                ;
+
+            resultado.aluno = aluno;
+            resultado.media = media;
+
+
+            if( media < 7)
+            {
+                resultado.situacao = "reprovado";
+            }
+            else
+            {
+                resultado.situacao = "aprovado";
+            }
+
+
+            return Ok(resultado);
+        }
+
+    }
+}
--- Exercicio/DTO/MediaAlunoDTO.cs
+++ Exercicio/DTO/MediaAlunoDTO.cs
@@ -2,5 +2,11 @@
 {
     public class MediaAlunoDTO
     {
+
+        public string? aluno { get; set; }
+
+        public double media { get; set; }
+
+        public string situacao { get; set; }
     }
 }
